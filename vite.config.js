8fe3569809040c058592dad511ec73318b540158
ssshooter <ssshooterx@gmail.com>
2023-01-27 21:12:37 +0800

    now can build preview page

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,31 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
+export default defineConfig(({ mode }) => {
+  console.log('mode', mode)
+  const config = {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['react', 'react-dom', 'mind-elixir'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {},
+        },
+      },
+    },
+  }
+  if (mode === 'production') {
+    config.build.lib = {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/MindElixirReact.jsx'),
       name: 'MindElixirReact',
       // the proper extensions will be added
       fileName: 'mind-elixir-react',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react', 'react-dom', 'mind-elixir'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {},
-      },
-    },
-  },
+    }
+  }
+  return config
 })
